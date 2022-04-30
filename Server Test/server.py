16b45a1aeaee353a36b75503b015f25768ff0af7
Axionix <73323166+Axionix@users.noreply.github.com>
2022-04-30 15:19:56 -0400

    Update server.py

--- Server Test/server.py
+++ Server Test/server.py
@@ -4,7 +4,6 @@ import threading
 HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
-print(socket.gethostbyname(socket.gethostname()))
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MSG = '[DISCONNECTED]'
